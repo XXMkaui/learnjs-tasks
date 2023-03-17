a32bb1458b2bf5395b735de776503b293d9bd290
XXMkaui <XXMkaui@proton.me>
2023-03-17 17:01:23 +0600

    part_2_api

---
+++ api/chapter_5/task_1.js
@@ -0,0 +1,9 @@
+// Accept: */*
+// Accept-Charset: utf-8
+// Accept-Encoding: gzip,deflate,sdch
+// Connection: keep-alive
+// Host: google.com
+// Origin: http://javascript.info
+// Referer: http://javascript.info/some/url
+
+// Refer может отсутствовать при fetch, поэтому нужен Origin, потому что Refer - необязательный заголовок; Origin же всегда отображается корректно при запросах
